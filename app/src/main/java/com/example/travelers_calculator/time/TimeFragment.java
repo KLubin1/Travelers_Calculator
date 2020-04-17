bc71076bb1d0eaeeb7146388a31a1d91ff8f7a8d
KLubin1 <KLubin1@student.fullsail.edu>
2020-04-17 11:04:58 -0400

    Added base time converter layout

--- app/src/main/java/com/example/travelers_calculator/time/TimeFragment.java
+++ app/src/main/java/com/example/travelers_calculator/time/TimeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +15,19 @@ import com.example.travelers_calculator.R;
 
 public class TimeFragment extends Fragment
 {
+    private TimePicker currentTime, convertTime;
+    private Spinner currentSpinner, convertSpinner;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_time, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View v= inflater.inflate(R.layout.fragment_time, container, false);
+        currentTime = v.findViewById(R.id.current_clock);
+        convertTime = v.findViewById(R.id.convert_clock);
+        currentSpinner = v.findViewById(R.id.current_spinner);
+        convertSpinner = v.findViewById(R.id.convert_spinner);
+
+        return v;
     }
 }
