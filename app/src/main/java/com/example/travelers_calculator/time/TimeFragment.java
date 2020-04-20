489562975c4a24368059d003a1c8e72f3785cb67
KLubin1 <KLubin1@student.fullsail.edu>
2020-04-20 14:15:02 -0400

    Added functionalty layout

--- app/src/main/java/com/example/travelers_calculator/time/TimeFragment.java
+++ app/src/main/java/com/example/travelers_calculator/time/TimeFragment.java
@@ -17,6 +17,9 @@ import androidx.fragment.app.Fragment;
 
 import com.example.travelers_calculator.R;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class TimeFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     private TimePicker currentTime, convertTime;
     private Spinner currentSpinner, convertSpinner;
@@ -45,7 +48,6 @@ public class TimeFragment extends Fragment implements AdapterView.OnItemSelected
         convertSpinner.setAdapter(adapter2);
         convertSpinner.setOnItemSelectedListener(this);
 
-
         return v;
     }
 
@@ -60,8 +62,75 @@ public class TimeFragment extends Fragment implements AdapterView.OnItemSelected
         //so theres no need for a convert button
         //and call the conversion factory and set the hour to its corresponding city or timezone
 
-        convertTime.setHour(conversionFactory("New York"));
+        //for current spinner
+        if(currentSpinner.getSelectedItemPosition() == 0)
+        {
+          //display current/local time
+            Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+            currentTime.setHour(hour);
+            currentTime.setMinute(minute);
+        }
+        else if(currentSpinner.getSelectedItemPosition()== 1)
+            currentTime.setHour(conversionFactory("New York"));
+        else if(currentSpinner.getSelectedItemPosition()== 2)
+            currentTime.setHour(conversionFactory("London"));
+        else if(currentSpinner.getSelectedItemPosition()== 3)
+            currentTime.setHour(conversionFactory("Los Angeles"));
+        else if(currentSpinner.getSelectedItemPosition() ==4)
+            currentTime.setHour(conversionFactory("Dubai"));
+        else if(currentSpinner.getSelectedItemPosition() ==5)
+            currentTime.setHour(conversionFactory("Paris"));
+        else if(currentSpinner.getSelectedItemPosition() ==6)
+            currentTime.setHour(conversionFactory("Moscow"));
+        else if(currentSpinner.getSelectedItemPosition() ==7)
+            currentTime.setHour(conversionFactory("Cairo"));
+        else if(currentSpinner.getSelectedItemPosition() ==8)
+            currentTime.setHour(conversionFactory("Beijing"));
+        else if(currentSpinner.getSelectedItemPosition() ==9)
+            currentTime.setHour(conversionFactory("Hong Kong"));
+        else if(currentSpinner.getSelectedItemPosition() ==10)
+            currentTime.setHour(conversionFactory("Brasilia"));
+        else if(currentSpinner.getSelectedItemPosition() ==11)
+            currentTime.setHour(conversionFactory("New Delhi"));
+        else if(currentSpinner.getSelectedItemPosition() ==12)
+            currentTime.setHour(conversionFactory("Mexico City"));
 
+        //for convert spinner
+        if(convertSpinner.getSelectedItemPosition() == 0)
+        {
+            //display current/local time
+            Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+            convertTime.setHour(hour);
+            convertTime.setMinute(minute);
+        }
+        else if(convertSpinner.getSelectedItemPosition()== 1)
+            convertTime.setHour(conversionFactory("New York"));
+        else if(convertSpinner.getSelectedItemPosition()== 2)
+            convertTime.setHour(conversionFactory("London"));
+        else if(convertSpinner.getSelectedItemPosition()== 3)
+            convertTime.setHour(conversionFactory("Los Angeles"));
+        else if(convertSpinner.getSelectedItemPosition() ==4)
+            convertTime.setHour(conversionFactory("Dubai"));
+        else if(convertSpinner.getSelectedItemPosition() ==5)
+            convertTime.setHour(conversionFactory("Paris"));
+        else if(convertSpinner.getSelectedItemPosition() ==6)
+            convertTime.setHour(conversionFactory("Moscow"));
+        else if(convertSpinner.getSelectedItemPosition() ==7)
+            convertTime.setHour(conversionFactory("Cairo"));
+        else if(convertSpinner.getSelectedItemPosition() ==8)
+            convertTime.setHour(conversionFactory("Beijing"));
+        else if(convertSpinner.getSelectedItemPosition() ==9)
+            convertTime.setHour(conversionFactory("Hong Kong"));
+        else if(convertSpinner.getSelectedItemPosition() ==10)
+            convertTime.setHour(conversionFactory("Brasilia"));
+        else if(convertSpinner.getSelectedItemPosition() ==11)
+            convertTime.setHour(conversionFactory("New Delhi"));
+        else if(convertSpinner.getSelectedItemPosition() ==12)
+            convertTime.setHour(conversionFactory("Mexico City"));
     }
 
     @Override
@@ -73,10 +142,45 @@ public class TimeFragment extends Fragment implements AdapterView.OnItemSelected
     //TODO: conversion factory
     //take in a string key for each city, then pass that in a switch case to return the correct hour difference
     //then pass that in the if-else ifs of onItemSelected
-    public int conversionFactory(String city_or_timezone)
+    public int conversionFactory(String city)
     {
-        //taking in the current time and adding/subtracting the offset
-        int offset = 4;
+        //Result of taking in the UTC time and adding/subtracting the offset
+        int offset = 0;
+        //Calendar c = Calendar.getInstance();
+        //int utcOffset = c.get(Calendar.ZONE_OFFSET) + c.get(Calendar.DST_OFFSET);
+
+        //Date::getTime allegedly uses UTC timezone.
+        Date d = new Date();
+        int utcTime = (int)d.getTime();
+
+        switch(city)
+        {
+            case "New York":
+                offset = utcTime-4;
+            case "London":
+                offset = utcTime+1;
+            case "Los Angeles":
+                offset = utcTime-7;
+            case "Dubai":
+                offset = utcTime+4;
+            case "Paris":
+                offset = utcTime+2;
+            case "Moscow":
+                offset = utcTime+3;
+            case "Cairo":
+                offset = utcTime+2;
+            case "Hong Kong":
+                offset = utcTime+8;
+            case "Beijing":
+                offset = utcTime+8;
+            case "New Delhi":
+                offset=utcTime+5;
+            case "Mexico City":
+                offset = utcTime-5;
+            case "Brasilia":
+                offset = utcTime-3;
+        }
+
         return offset;
 
     }
