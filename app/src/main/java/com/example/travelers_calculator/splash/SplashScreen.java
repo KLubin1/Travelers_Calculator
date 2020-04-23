ea4861a3a8ab8cabbcc7f787bbf356efcc328686
KLubin1 <KLubin1@student.fullsail.edu>
2020-04-22 20:47:29 -0400

    Added logo animation for splash screen

--- app/src/main/java/com/example/travelers_calculator/splash/SplashScreen.java
+++ app/src/main/java/com/example/travelers_calculator/splash/SplashScreen.java
@@ -24,17 +24,20 @@ public class SplashScreen extends AppCompatActivity
                 .withTargetActivity(MainActivity.class)
                 .withSplashTimeOut(3000)
                 .withBackgroundColor(Color.parseColor("#03DAC5"))
-                //.withHeaderText("Header")
-                //.withFooterText("Footer")
-                //.withBeforeLogoText("Before Logo Text")
-                .withAfterLogoText("Traveler's Calculator Demo")
+                .withAfterLogoText("Traveler's Calculator")
                 .withLogo(R.mipmap.ic_logo_foreground);
 
         getSupportActionBar().hide();
+        //animating logo
+        config.getLogo()
+                .animate()
+                .rotation(360)
+                .setStartDelay(800)
+                .setDuration(1500);
+        //text color
         config.getAfterLogoTextView().setTextColor(Color.WHITE);
+        //render
         View splashScreen = config.create();
         setContentView(splashScreen);
-
-
     }
 }
